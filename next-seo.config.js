3be3297b2145d6044a0a35d26e09acea29989088
Dotun-Balogun <balogunlightonolamidotun@gmail.com>
2025-11-11 21:46:37 +0100

    project setup:theme setup, seo configuration, folder structure , install dependencies

---
+++ next-seo.config.js
@@ -0,0 +1,23 @@
+export default {
+  title: "Grandmax Construction",
+  description: "We build your dreams — Grandmax Construction Company.",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://grandmax.vercel.app/",
+    siteName: "Grandmax Construction",
+    images: [
+      {
+        url: "/og-image.png", // located in public/og-image.png
+        width: 1200,
+        height: 630,
+        alt: "Grandmax Construction Project Showcase",
+      },
+    ],
+  },
+  twitter: {
+    handle: "@grandmax",
+    site: "@grandmax",
+    cardType: "summary_large_image",
+  },
+};
---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+// next-sitemap.config.js
+module.exports = {
+  siteUrl: 'https://yoursite.com',
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+}
