a87ac5d535a3de35a6dafb417eade1ac490ef77e
Dotun-Balogun <balogunlightonolamidotun@gmail.com>
2025-11-27 09:59:57 +0100

    contact page

--- app/(pages)/contact/page.tsx
+++ app/(pages)/contact/page.tsx
@@ -4,13 +4,10 @@ import ContactForm from '@/app/auth/ContactForm'
 
 const ContactPage = () => {
   return (
-    <div className="min-h-screen bg-primary text-whit">
+    <div className="min-h-screen  text-whit">
       <div className="max-w-7xl mx-auto px-4 py-12">
-        <h1 className="text-3xl font-semibold mb-6">Contact</h1>
 
-        {/* responsive two-column: form on the left, map on the right */}
         <div className="flex flex-col md:flex-row gap-6">
-          {/* Contact form card */}
           <div className="md:w-1/2 bg-secondary p-6 rounded-lg shadow-md flex flex-col">
             <p className="mb-4 text-gray-100">
               Send us a message and we&apos;ll get back to you as soon as possible.
