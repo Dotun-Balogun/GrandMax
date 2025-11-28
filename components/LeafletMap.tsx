a487b05a26426ac7ef863e49d219b517a06981b5
Dotun-Balogun <balogunlightonolamidotun@gmail.com>
2025-11-28 17:56:28 +0100

    bottom navigation

--- components/LeafletMap.tsx
+++ components/LeafletMap.tsx
@@ -32,7 +32,7 @@ export const LeafletMap: React.FC<LeafletMapProps> = ({ lat, lng, zoom }) => {
         }).addTo(map);
 
         const customIcon = L.divIcon({
-          html: `<div class="bg-primary w-10 h-10 rounded-full flex items-center justify-center shadow-lg border-4 border-white">
+          html: `<div class="bg-primary -z-10 w-10 h-10 rounded-full flex items-center justify-center shadow-lg border-4 border-white">
                    <svg class="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20">
                      <path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd"/>
                    </svg>
