545f761424297dc6e48824dc6f84bba199acf943
Dotun-Balogun <balogunlightonolamidotun@gmail.com>
2025-11-30 14:30:04 +0100

    added building project page

---
+++ app/buildingProjects/page.tsx
@@ -0,0 +1,13 @@
+import BuildingGrid from '@/components/sections/Home/BuildingGrid'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='max-w-7xl px-4 mx-auto py-16'>
+        <BuildingGrid/>
+
+    </div>
+  )
+}
+
+export default page
