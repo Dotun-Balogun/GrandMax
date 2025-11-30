c29117af6bd60257bb7e1f1aa47013ed3acaf12d
Dotun-Balogun <balogunlightonolamidotun@gmail.com>
2025-11-30 09:40:35 +0100

    testimonial correction

--- components/sections/Home/TestimonialSection.tsx
+++ components/sections/Home/TestimonialSection.tsx
@@ -14,7 +14,7 @@ import bakcground from '@/app/assets/images/row-bg-1.jpg'
 
 const TestimonialSection = () => {
   return (
-    <section className=" relative w-full mt-100  lg:mt-0 ">*+
+    <section className=" relative w-full mt-100  lg:mt-0 ">
         
        <section className="absolute left-1/2  bottom-20 -translate-x-1/2 w-full flex flex-col lg:flex-row lg:px-10 z-10" >
       {/* LEFT RED SECTION */}
