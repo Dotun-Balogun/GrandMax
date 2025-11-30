d6fca2b9f46212a7d418349c641b23de71604343
Dotun-Balogun <balogunlightonolamidotun@gmail.com>
2025-11-30 09:01:41 +0100

    adjusted architecutre padding

--- components/sections/Home/LatestPost.tsx
+++ components/sections/Home/LatestPost.tsx
@@ -7,9 +7,9 @@ const LatestPost = () => {
         <div className='w-full'>
       <div className="max-w-7xl  px-18 ">
         <div className=' space-y-2'>
-          <p className="text-[14px] text-center md:text-left text-gray-500">WHAT DO WE DO HERE</p>
+          <p className="text-[14px] text-center md:text-left text-gray-500">Latest Posts</p>
           <h1 className='text-[32px] white-space-break lg:text-[42px] tracking-loose text-center  md:text-left font-bold'>
-            what  make us
+           Latest Articles 
 
           </h1>
         </div>
@@ -19,9 +19,9 @@ const LatestPost = () => {
       <div className='bg-secondary  lg:ml-10'>
         <div className="px-8">
           <h1 className='text-[32px] white-space-break lg:text-[42px] tracking-loose  border-b text-center text-white  md:text-left font-bold'>
-            Different</h1>
+            Architecture</h1>
         </div>
-        <div className="flex flex-col md:flex-row px-8">
+        <div className="flex flex-col md:flex-row ">
           <div className="text-white">
 <BlogCarousel 
         posts={featuredPosts}
