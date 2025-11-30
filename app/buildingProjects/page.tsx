4fcf656bf1a95f1e1a3813562386b82fa90cd3a1
Dotun-Balogun <balogunlightonolamidotun@gmail.com>
2025-11-30 14:36:09 +0100

    added metadata for building project page

--- app/buildingProjects/page.tsx
+++ app/buildingProjects/page.tsx
@@ -1,5 +1,14 @@
 import BuildingGrid from '@/components/sections/Home/BuildingGrid'
+import { generatePageMetadata } from '@/lib/data/metadata'
+import { title } from 'process'
 import React from 'react'
+
+
+const metadata = generatePageMetadata(
+     'Building Projects',
+     'Explore our diverse range of building projects showcasing our expertise in architecture and construction.',
+    '/buildingProjects'
+)
 
 const page = () => {
   return (
